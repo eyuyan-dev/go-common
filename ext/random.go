b42ce1f2e496dc33a336651f1199222e864fdbbd
rootsuper <9889591@qq.com>
2020-01-05 19:36:20 +0800

    add RangeRand

--- ext/random.go
+++ ext/random.go
@@ -1,7 +1,10 @@
 package ext
 
 import (
-	"math/rand"
+	"crypto/rand"
+	"math"
+	"math/big"
+	Rand "math/rand"
 	"time"
 )
 
@@ -9,9 +12,26 @@ func GetRandomString(length int) string {
 	str := "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	bytes := []byte(str)
 	var result []byte
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	r := Rand.New(Rand.NewSource(time.Now().UnixNano()))
 	for i := 0; i < length; i++ {
 		result = append(result, bytes[r.Intn(len(bytes))])
 	}
 	return string(result)
+}
+
+func RangeRand(min, max int64) int64 {
+	if min > max {
+		panic("the min is greater than max!")
+	}
+
+	if min < 0 {
+		f64Min := math.Abs(float64(min))
+		i64Min := int64(f64Min)
+		result, _ := rand.Int(rand.Reader, big.NewInt(max+1+i64Min))
+
+		return result.Int64() - i64Min
+	} else {
+		result, _ := rand.Int(rand.Reader, big.NewInt(max-min+1))
+		return min + result.Int64()
+	}
 }
